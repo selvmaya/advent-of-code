8f99b91d828371759d4ca2e94c35d0c25b64d1f4
Maya <selvmaya@proton.me>
2025-12-04 17:06:26 +0100

    scaffolding for d03

---
+++ aoc2025/d03/src/p1.rs
@@ -0,0 +1,14 @@
+use winnow::{prelude::*, Result as WResult};
+
+fn main() {
+    println!("Hello, world!");
+}
+
+struct Solution();
+struct Puzzle();
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn example_input_works() {}
+}
